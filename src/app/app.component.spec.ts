3b8e868bbfa9749bfd5d3a6b12375f7d91edb329
_whyvrafvr <jsie@trendev.fr>
2022-03-09 14:50:39 +0100

    clean default config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,10 +30,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual(title);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(`${title} is coming...`);
-  });
 });
