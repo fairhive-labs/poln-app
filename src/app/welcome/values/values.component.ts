fa89cc8ffe3a65392f13de8d7d03995856786ff5
_whyvrafvr <jsie@trendev.fr>
2022-03-16 15:08:54 +0100

    fill community

--- src/app/welcome/values/values.component.ts
+++ src/app/welcome/values/values.component.ts
@@ -7,14 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValuesComponent implements OnInit {
 
-  option = true
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  changeOption() {
-    this.option = !this.option;
   }
 
 }
