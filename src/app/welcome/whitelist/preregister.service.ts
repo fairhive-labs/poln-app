ca6f38ae83816bafc09c4dc6a8813ea9d4988eb2
_whyvrafvr <jsie@trendev.fr>
2022-04-06 17:34:36 +0200

    mock  PreregisterService

--- src/app/welcome/whitelist/preregister.service.ts
+++ src/app/welcome/whitelist/preregister.service.ts
@@ -1,15 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreregisterService {
 
-  url = "http://preregister.fairhive.io"
+  private url = "https://polar-plains-98105.herokuapp.com/";
+
   constructor(private http: HttpClient) { }
 
-  register(address: string, email: string, utype: string) { }
+  register(address: string, email: string, utype: string) {
+    console.log('POST /');
+    return of({ hash: "H45h" });
+  }
 
-  activate(token: string, hash: string) { }
+  activate(token: string, hash: string) {
+    console.log(`POST /activate/${token}/${hash}`);
+    return of({ token: "t0k3N", activated: true });
+  }
 }
