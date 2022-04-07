4ad419d70b8cfdc2f0630637c32b87faff3ebc48
_whyvrafvr <jsie@trendev.fr>
2022-04-07 16:49:21 +0200

    test 400 bad request

--- src/app/welcome/whitelist/preregister.service.spec.ts
+++ src/app/welcome/whitelist/preregister.service.spec.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
@@ -36,18 +37,37 @@ describe('PreregisterService', () => {
   it('can POST to register endpoint', () => {
     const url = service.url;
     const testData: RegisterResponse = { hash: 'h4Sh' };
-    const user = {
-      email: 'jsie@trendev.fr',
-      address: '0x8ba1f109551bD432803012645Ac136ddd64DBA72',
-      type: 'mentor'
-    };
+    const email = 'jsie@trendev.fr';
+    const address = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+    const type = 'mentor';
 
-    service.register(user.address, user.email, user.type)
-      .subscribe(r => expect(r).toEqual(testData));
-
+    service.register(address, email, type).subscribe(r => expect(r).toEqual(testData));
     const req = httpTestingController.expectOne(`${url}/`);
     expect(req.request.method).toEqual('POST');
     req.flush(testData);
+  });
+
+  it('should fail', () => {
+    const url = service.url;
+    const email = 'jsie@trendev.fr';
+    const address = '0x';
+    const type = 'mentor';
+    const err = { error: 'bad address' }
+
+    httpClient.post<RegisterResponse>(`${url}/`, {
+      address: address,
+      email: email,
+      type: type,
+    }).subscribe({
+      next: () => fail('should have failed with the 400 error'),
+      error: (error: HttpErrorResponse) => {
+        expect(error.status).withContext('status').toEqual(400);
+        expect(error.error).withContext('message').toEqual(err);
+      },
+    });
+    const req = httpTestingController.expectOne(`${url}/`);
+    req.flush(err, { status: 400, statusText: 'Bad Request' });
+
   });
 
   it('can POST to activate endpoint', () => {
