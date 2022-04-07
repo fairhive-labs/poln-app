2a9945457f291392b3817c3a32f31350d2c2686f
_whyvrafvr <jsie@trendev.fr>
2022-04-07 12:56:31 +0200

    improve preregister tests

--- src/app/welcome/whitelist/preregister.service.spec.ts
+++ src/app/welcome/whitelist/preregister.service.spec.ts
@@ -2,27 +2,63 @@ import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
-import { PreregisterService } from './preregister.service';
+import { PreregisterService, RegisterResponse, ActivateResponse } from './preregister.service';
 
 describe('PreregisterService', () => {
   let service: PreregisterService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    let httpClient: HttpClient;
-    let httpTestingController: HttpTestingController;
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
       ]
     });
-    // Inject the http service and test controller for each test
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
-    
+
     service = TestBed.inject(PreregisterService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should contain a valid url', () => {
+    expect(service.url).toBeTruthy();
+  });
+
+  it('can POST to register endpoint', () => {
+    const url = service.url;
+    const testData: RegisterResponse = { hash: 'h4Sh' };
+    const user = {
+      email: 'jsie@trendev.fr',
+      address: '0x8ba1f109551bD432803012645Ac136ddd64DBA72',
+      type: 'mentor'
+    };
+
+    service.register(user.address, user.email, user.type)
+      .subscribe(r => expect(r).toEqual(testData));
+
+    const req = httpTestingController.expectOne(`${url}/`);
+    expect(req.request.method).toEqual('POST');
+    req.flush(testData);
+  });
+
+  it('can POST to activate endpoint', () => {
+    const url = service.url;
+    const token = 't0k3N';
+    const hash = 'h4Sh';
+    const testData: ActivateResponse = { token: token, activated: true };
+
+    service.activate(token, hash).subscribe(r => expect(r).toEqual(testData));
+    const req = httpTestingController.expectOne(`${url}/activate/${token}/${hash}`);
+    expect(req.request.method).toEqual('POST');
+    req.flush(testData);
   });
 });
--- src/app/welcome/whitelist/preregister.service.ts
+++ src/app/welcome/whitelist/preregister.service.ts
@@ -1,40 +1,33 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of, retry } from 'rxjs';
+import { catchError, Observable, of, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreregisterService {
 
-  private url = "https://polar-plains-98105.herokuapp.com/";
+  readonly url = "https://polar-plains-98105.herokuapp.com";
 
   constructor(private http: HttpClient) { }
 
-  register(user: User): Observable<RegisterResponse | ErrorResponse> {
-    return this.http.post<RegisterResponse | ErrorResponse>(`${this.url}`, user).pipe(
-      retry(3),
-      catchError(err => this.handleError(err, {}))
-    );
+  register(address: string, email: string, type: string): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(`${this.url}/`, {
+      address: address,
+      email: email,
+      type: type,
+    })
+      .pipe(
+        retry(3)
+      );
   }
 
   activate(token: string, hash: string) {
-    console.log(`POST /activate/${token}/${hash}`);
-    return of({ token: "t0k3N", activated: true });
+    return this.http.post<ActivateResponse>(`${this.url}/activate/${token}/${hash}`, null)
+      .pipe(
+        retry(3)
+      );
   }
-
-  private handleError(error: HttpErrorResponse, data: any) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
-    } else {
-      console.error(`Backend returned code ${error.status}, body was: `, error.error);
-    }
-    return of(data);
-  }
-}
-
-export interface ErrorResponse {
-  error: string;
 }
 
 export interface RegisterResponse {
@@ -44,10 +37,4 @@ export interface RegisterResponse {
 export interface ActivateResponse {
   activated: boolean;
   token: string;
-}
-
-export interface User {
-  address: string;
-  email: string;
-  type: string;
 }
