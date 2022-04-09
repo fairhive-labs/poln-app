8f7800a4a67562dad3860b6eaf18cade5f7e4e88
_whyvrafvr <jsie@trendev.fr>
2022-04-09 12:30:19 +0200

    improve register comp. tests

--- src/app/welcome/waitlist/register/register.component.spec.ts
+++ src/app/welcome/waitlist/register/register.component.spec.ts
@@ -1,5 +1,5 @@
 import { MatCardModule } from '@angular/material/card';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { MatInputModule } from '@angular/material/input';
@@ -8,12 +8,15 @@ import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Observable, of, throwError } from 'rxjs';
+import { PreregisterService } from '../preregister.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-
+  let preregisterService: jasmine.SpyObj<PreregisterService>;
   beforeEach(async () => {
+    preregisterService = jasmine.createSpyObj('PreregisterService', ['register']);
     await TestBed.configureTestingModule({
       imports: [
         NoopAnimationsModule,
@@ -24,7 +27,8 @@ describe('RegisterComponent', () => {
         HttpClientTestingModule,
         MatProgressBarModule,
       ],
-      declarations: [RegisterComponent]
+      declarations: [RegisterComponent],
+      providers: [{ provide: PreregisterService, useValue: preregisterService }]
     })
       .compileComponents();
   });
@@ -43,4 +47,58 @@ describe('RegisterComponent', () => {
     expect(component.types).toBeTruthy();
     expect(component.types).toHaveSize(7);
   });
+
+  it('should contain 3 formcontrol in form group', () => {
+    expect(Object.keys(component.controls)).toHaveSize(3);
+  });
+
+  it('should call submit()', fakeAsync(() => {
+    const registerResponse = {
+      hash: 'h4Sh'
+    }
+    preregisterService.register.and.returnValue(of(registerResponse));
+
+    const email = 'jsie@trendev.fr';
+    const address = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+    const type = 'mentor';
+
+    component.address.setValue(address);
+    component.email.setValue(email);
+    component.type.setValue(type);
+    expect(component.preregistrationForm.valid).toBeTrue();
+    component.submit();
+
+    tick();
+    fixture.detectChanges();
+
+    expect(component.progressing).toBeFalse();
+    expect(component.submitted).toBeTrue();
+    expect(component.submissionError).toBeFalsy();
+    expect(component.address.enabled).toBeTrue();
+    expect(component.email.enabled).toBeTrue();
+    expect(component.type.enabled).toBeTrue();
+  }));
+
+  it('should call submit() but display an error', fakeAsync(() => {
+    preregisterService.register.and.returnValue(throwError(() => new Error('something goes wrong')));
+
+    const email = 'jsie@trendev.fr';
+    const address = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+    const type = 'mentor';
+
+    component.address.setValue(address);
+    component.email.setValue(email);
+    component.type.setValue(type);
+    expect(component.preregistrationForm.valid).toBeTrue();
+    component.submit();
+
+    tick();
+    fixture.detectChanges();
+    expect(component.progressing).toBeFalse();
+    expect(component.submitted).toBeFalse();
+    expect(component.submissionError).toBeTruthy();
+    expect(component.address.enabled).toBeTrue();
+    expect(component.email.enabled).toBeTrue();
+    expect(component.type.enabled).toBeTrue();
+  }));
 });
