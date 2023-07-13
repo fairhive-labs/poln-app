94b8b92ac8b4e7fe7431c884d42a43470adb287a
_whyvrafvr <jsie@trendev.fr>
2023-07-13 12:02:25 +0200

    clean imports

--- src/app/welcome/waitlist/preregister.service.ts
+++ src/app/welcome/waitlist/preregister.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
-import { Observable, map, retry } from 'rxjs';
+import { map, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
